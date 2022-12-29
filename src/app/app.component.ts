9e1cfce3d3614dcd973668d1c454cace2f9e3b9e
mohammad <mohammad.hamzah.slo@gmail.com>
2022-12-30 00:57:51 +0100

    upload files

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  cityName : string = 'tokyo';
+  weatherData!: WeatherData;
+
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName = ''
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+
+  }
+
+  private getWeatherData (cityName: string) {
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next: (response) => {
+        this.weatherData = response;
+      }
+    })
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  appMainUrl : 'https://yahoo-weather5.p.rapidapi.com/weather',
+  hostHeaderName: 'X-RapidAPI-Host',
+  hostHeaderValue: 'yahoo-weather5.p.rapidapi.com',
+  keyHeaderName: 'X-RapidAPI-Key',
+  keyHeaderValue: 'f18f06c82amshdb9d27319246150p1724e7jsne5ce78b40bb1'
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
